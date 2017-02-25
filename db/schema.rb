e0bee7b975b00d13034dc1a15b11b218fdc28d1f
Korey Enright <kenright19@gmail.com>
2017-02-25 16:53:42 -0600

    Sets up devise user model
    closes #9

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170225221252) do
+ActiveRecord::Schema.define(version: 20170225225204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "campaigns", force: :cascade do |t|
+    t.string   "name",            null: false
+    t.text     "description"
+    t.integer  "organization_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["organization_id"], name: "index_campaigns_on_organization_id", using: :btree
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.string   "title",           null: false
+    t.text     "description"
+    t.string   "website"
+    t.integer  "min_player_size"
+    t.integer  "max_player_size"
+    t.string   "run_time"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  create_table "models", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_models_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true, using: :btree
+  end
 
   create_table "organizations", force: :cascade do |t|
     t.string   "name",        null: false
@@ -22,4 +59,5 @@ ActiveRecord::Schema.define(version: 20170225221252) do
     t.datetime "updated_at",  null: false
   end
 
+  add_foreign_key "campaigns", "organizations"
 end
