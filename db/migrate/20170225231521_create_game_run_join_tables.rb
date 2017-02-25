704f57b9d2616075d5fca05c7fc2ec71427f15a0
Korey Enright <kenright19@gmail.com>
2017-02-25 17:32:16 -0600

    Adds join tables
    adds
    run players
    run npcs
    character editors

--- db/migrate/20170225231521_create_game_run_join_tables.rb
+++ db/migrate/20170225231521_create_game_run_join_tables.rb
@@ -24,5 +24,29 @@ class CreateGameRunJoinTables < ActiveRecord::Migration[5.0]
       t.timestamps
     end
 
+    create_table :run_has_players do |t|
+      t.references :game     , index: true, foreign_key: true, null: false
+      t.references :editor   , index: true, foreign_key: { to_table: :users }, null: false
+      t.references :character, index: true, foreign_key: true, null: false
+      t.boolean    :private
+
+      t.timestamps
+    end
+
+    create_table :run_has_npcs do |t|
+      t.references :run      , index: true, foreign_key: true, null: false
+      t.references :npc      , index: true, foreign_key: { to_table: :users }, null: false
+      t.references :character, index: true, foreign_key: true, null: false
+      t.boolean    :private
+
+      t.timestamps
+    end
+
+    create_table :characters_have_editors do |t|
+      t.references :character, index: true, foreign_key: true, null: false
+      t.references :editor, index: true, foreign_key: { to_table: :users }, null: false
+      t.timestamps
+    end
+
   end
 end
