14a7ce93cbb81c3378022f549453dd0687a3790b
Korey Enright <kenright19@gmail.com>
2017-02-25 16:22:46 -0600

    Finishes adding campaigns datatable
    closes #11

--- db/migrate/20170225221634_create_campaigns.rb
+++ db/migrate/20170225221634_create_campaigns.rb
@@ -1,7 +1,10 @@
 class CreateCampaigns < ActiveRecord::Migration[5.0]
   def change
     create_table :campaigns do |t|
-
+      t.string     :name,         null: false
+      t.text       :description,  null: false
+      t.references :organization, index: true, foreign_key: true
+      
       t.timestamps
     end
   end
