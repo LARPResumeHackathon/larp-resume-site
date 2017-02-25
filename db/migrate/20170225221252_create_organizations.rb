737a8b6f232555e1a4adee60a3d40ea1eb66d343
Korey Enright <kenright19@gmail.com>
2017-02-25 16:13:48 -0600

    Creates the Organization model
    closes #10

---
+++ db/migrate/20170225221252_create_organizations.rb
@@ -0,0 +1,10 @@
+class CreateOrganizations < ActiveRecord::Migration[5.0]
+  def change
+    create_table :organizations do |t|
+      t.string :name,        null: false
+      t.text   :description, null: false
+
+      t.timestamps
+    end
+  end
+end
