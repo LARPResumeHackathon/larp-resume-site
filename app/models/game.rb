b9ab142155072cb8a9c16207f5709277b3f37b2d
Korey Enright <kenright19@gmail.com>
2017-02-26 14:31:48 -0600

    Adds game associations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,9 @@
 class Game < ApplicationRecord
+  has_many :runs
+  has_and_belongs_to_many :editors, class_name: "User",
+                          join_table: :games_editors,
+                          association_foreign_key: :editor_id
+  has_and_belongs_to_many :authors, class_name: "User",
+                          join_table: :games_authors,
+                          association_foreign_key: :author_id
 end
