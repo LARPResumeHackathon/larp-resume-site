c94078b2826a4b633dd2514dfb8be754e8c7bc5a
Korey Enright <kenright19@gmail.com>
2017-02-26 14:32:31 -0600

    Adds run associations

--- app/models/run.rb
+++ app/models/run.rb
@@ -1,2 +1,15 @@
 class Run < ApplicationRecord
+  belongs_to :game
+  has_and_belongs_to_many :editors, class_name: "User",
+                          join_table: :runs_editors,
+                          association_foreign_key: :editor_id
+  has_and_belongs_to_many :gms, class_name: "User",
+                          join_table: :runs_gms,
+                          association_foreign_key: :gm_id
+  has_and_belongs_to_many :players, class_name: "User",
+                          join_table: :runs_players,
+                          association_foreign_key: :player_id
+  has_and_belongs_to_many :npcs, class_name: "User",
+                          join_table: :runs_npcs,
+                          association_foreign_key: :npc_id
 end
