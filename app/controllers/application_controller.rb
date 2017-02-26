f937e49b3e8ec2f750e65c3fea10d31c4e143a41
cmho <carlymho@gmail.com>
2017-02-26 13:43:54 -0600

    make it possible to actually register

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:display_name])
+    devise_parameter_sanitizer.permit(:edit, keys: [:display_name, :about_me])
+  end
 end
