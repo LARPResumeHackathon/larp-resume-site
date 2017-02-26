07b6966ce6eff4c349ace7de8c9d39ba1987d5e2
Korey Enright <kenright19@gmail.com>
2017-02-26 14:28:58 -0600

    Adds model associations to campaigns
    closes #67

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,2 +1,6 @@
 class Campaign < ApplicationRecord
+  has_many :sessions
+  has_and_belongs_to_many :editors, class_name: "User",
+                          join_table: :campaigns_editors,
+                          association_foreign_key: :editor_id
 end
