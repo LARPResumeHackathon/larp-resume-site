eb22073e75ba7f5959c2879147097e9ec5fb4046
Carly Ho <cho@cliquestudios.com>
2017-07-21 11:14:10 -0500

    remove line of code that wasn't working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @authored = Game.where(:authors).include(params[:id])
+
+    # I have nfc how the runs/campaigns/games/sessions thing works so I'll leave that to someone who designed the database to connect some dots here, ahaha.
   end
 end
