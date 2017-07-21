818d80b65086d0d90f6c7443182c565c790b77a0
Carly Ho <cho@cliquestudios.com>
2017-07-21 10:46:01 -0500

    Add home controller, characters controller and campaigns controller, plus routing, handlers and views

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "home#index"
-
+  resources :campaigns
+  resources :characters, except: :index
+  resources :users
+  resources :games
 end
